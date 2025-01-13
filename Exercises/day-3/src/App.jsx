5afcbb5b799286ca586cb8a4cc8722f420b2c6c2
Fitsumhelina <permin1501424@gmail.com>
2025-01-13 21:55:48 +0300

    tailwind installed

--- Exercises/day-3/src/App.jsx
+++ Exercises/day-3/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
-    
+      <Home />
     </>
   )
 }
--- Exercises/day-3/src/Home.jsx
+++ Exercises/day-3/src/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-
 
 function Home() {
-  return (
+    const techs = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js']
+   
+    return (
     <div>
-
+        <img src="https://avatars.githubusercontent.com/u/151903384?v=4" alt="" id='profile_pic' />
+        <h3>fitsum helina <span><img src="https://cdn-icons-png.flaticon.com/128/6270/6270515.png" alt="" id='bullet' /></span></h3>
+        <ul id='ul'>
+            {techs.map((tech, i) => {
+                return <li key={i} id='li'>{tech}</li>
+            })}
+        </ul>
     </div>
   )
 }
